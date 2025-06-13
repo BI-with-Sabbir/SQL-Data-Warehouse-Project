90de2c4b33d30dc8c8552c8086611f701e543ad0
Sabbir Hossain Rossi <shabbir.biforbusiness@gmail.com>
2025-06-13 15:44:13 +0600

    Update create_database.sql

--- Scripts/create_database.sql
+++ Scripts/create_database.sql
@@ -1,1 +1,15 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'. 
+    Optionally, it drops and recreates the database if it already exists. 
+    Within the database, it sets up three schemas: 'bronze', 'silver', and 'gold'.
 
+WARNING:
+    If @RecreateDB is set to 1, the existing 'DataWarehouse' database will be DROPPED,
+    and ALL data will be permanently deleted.
+    Ensure backups are in place before running this script with @RecreateDB = 1.
+=============================================================
+*/
